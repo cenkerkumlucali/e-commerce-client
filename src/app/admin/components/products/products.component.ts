6120b84f769dbe83060c8d2394525ca3f66a8d37
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-10-16 23:44:16 +0300

    added product component and model class

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {BaseComponent, SpinnerType} from "../../../base/base.component";
 import {NgxSpinnerService} from "ngx-spinner";
+import {HttpClientService} from "../../../services/common/http-client.service";
+import {CreateProduct} from "../../../contracts/create-product";
+import {ListComponent} from "./list/list.component";
 
 @Component({
   selector: 'app-products',
@@ -9,12 +12,17 @@ import {NgxSpinnerService} from "ngx-spinner";
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
 
-  constructor(spinner:NgxSpinnerService) {
+  constructor(spinner: NgxSpinnerService,
+              private httpService: HttpClientService) {
     super(spinner);
   }
 
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom);
   }
 
+  @ViewChild(ListComponent) listComponents: ListComponent;
+
+  createdProduct(createdProduct: CreateProduct) {
+    this.listComponents.getProducts();
+  }
 }
--- src/app/admin/components/products/products.module.ts
+++ src/app/admin/components/products/products.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ProductsComponent } from './products.component';
+import {Directive, NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ProductsComponent} from './products.component';
 import {RouterModule} from "@angular/router";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {CreateComponent} from './create/create.component';
+import {ListComponent} from './list/list.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {DeleteDirective} from "../../../directives/admin/delete.directive";
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    CreateComponent,
+    ListComponent,
+    DeleteDirective
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       {path: "", component: ProductsComponent}
-    ])
+    ]),
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule
   ]
+
 })
-export class ProductsModule { }
+
+export class ProductsModule {
+}
