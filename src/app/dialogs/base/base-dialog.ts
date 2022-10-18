0adcc7b6b16d86854922ce6e8299572c24134feb
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-10-18 23:38:50 +0300

    created Base Dialog class

---
+++ src/app/dialogs/base/base-dialog.ts
@@ -0,0 +1,9 @@
+import {MatDialogRef} from "@angular/material/dialog";
+
+export class BaseDialog<T> {
+  constructor(public dialogRef: MatDialogRef<T>) {
+  }
+  close(){
+    this.dialogRef.close();
+  }
+}
