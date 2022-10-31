740cb7a9558a78e9b664bd79e23ec0cf9372d732
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-10-31 23:32:41 +0300

    add MatButtonModule

--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -3,18 +3,20 @@ import {CommonModule} from '@angular/common';
 import {DeleteDialogComponent} from "./delete-dialog/delete-dialog.component";
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatButtonModule} from "@angular/material/button";
-import {FileUploadDialogComponent} from "./file-upload-dialog/file-upload-dialog.component";
+import {SelectProductImageDialogComponent} from './select-product-image-dialog/select-product-image-dialog.component';
+import {FileUploadModule} from "../services/common/file-upload/file-upload.module";
+import {MatCardModule} from "@angular/material/card";
 
 
 @NgModule({
   declarations: [
     DeleteDialogComponent,
-    FileUploadDialogComponent
+    SelectProductImageDialogComponent
   ],
   imports: [
     CommonModule,
-    MatButtonModule,
-    MatDialogModule
+    FileUploadModule,
+    MatButtonModule, MatDialogModule, MatCardModule
   ]
 })
 export class DialogModule {
