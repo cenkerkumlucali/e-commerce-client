4c31cba2051cf78b9d55a675cd472e5ec12c4e43
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-08 01:09:56 +0300

    created Global http error handler interceptor service

--- src/app/services/common/http-error-handler-interceptor.service.ts
+++ src/app/services/common/http-error-handler-interceptor.service.ts
@@ -3,13 +3,19 @@ import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpStatusCode} fr
 import {catchError, Observable, of} from "rxjs";
 import {CustomToastrService, ToastrMessageType, ToastrPosition} from "../ui/custom-toastr.service";
 import {UserAuthService} from "./models/user-auth.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {NgxSpinnerService} from "ngx-spinner";
+import {SpinnerType} from "../../base/base.component";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpErrorHandlerInterceptorService implements HttpInterceptor {
 
-  constructor(private toastrService: CustomToastrService, private userAuthService: UserAuthService) {
+  constructor(private toastrService: CustomToastrService,
+              private userAuthService: UserAuthService,
+              private router:Router,
+              private spinner:NgxSpinnerService) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -17,11 +23,22 @@ export class HttpErrorHandlerInterceptorService implements HttpInterceptor {
     return next.handle(req).pipe(catchError(error => {
       switch (error.status) {
         case HttpStatusCode.Unauthorized:
-          this.toastrService.message("Bu işlemi yapmaya yetkiniz bulunmamaktadır.", "Yetkisiz İşlem", {
-            messageType: ToastrMessageType.Warning,
-            position: ToastrPosition.BottomFullWidth
-          });
-          this.userAuthService.refreshTokenLogin(localStorage.getItem("refreshToken")).then(data => {
+
+          this.userAuthService.refreshTokenLogin(localStorage.getItem("refreshToken"),(state)=>{
+            if(!state){
+              const url = this.router.url;
+              if(url == "/products")
+                this.toastrService.message("Sepete ürün eklemeniz için oturum açmanız gerekiyor.", "Oturum Açınız", {
+                  messageType: ToastrMessageType.Warning,
+                  position: ToastrPosition.TopRight
+                });
+              else
+                this.toastrService.message("Bu işlemi yapmaya yetkiniz bulunmamaktadır.", "Yetkisiz İşlem", {
+                  messageType: ToastrMessageType.Warning,
+                  position: ToastrPosition.BottomFullWidth
+                });
+            }
+          }).then(data => {
           });
           break;
         case HttpStatusCode.InternalServerError:
@@ -49,6 +66,7 @@ export class HttpErrorHandlerInterceptorService implements HttpInterceptor {
           });
           break;
       }
+      this.spinner.hide(SpinnerType.BallAtom);
       return of(error);
     }));
   }
