075707c5b3989a2f065ed5d4466cf94e212a0eeb
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-10 12:39:14 +0300

    added PasswordResetModule and UpdatePasswordModule

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -4,18 +4,20 @@ import {BasketsModule} from "./baskets/baskets.module";
 import {HomeModule} from "./home/home.module";
 import {ProductsModule} from "./products/products.module";
 import {RegisterModule} from "./register/register.module";
-import {LoginModule} from "./login/login.module";
+import {PasswordResetModule} from "./password-reset/password-reset.module";
+import {UpdatePasswordModule} from "./update-password/update-password.module";
 
 
 @NgModule({
-  declarations: [
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     BasketsModule,
     HomeModule,
     ProductsModule,
     RegisterModule,
+    PasswordResetModule,
+    UpdatePasswordModule
     // LoginModule
   ],
   exports: [
