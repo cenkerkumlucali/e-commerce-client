2957bdd9bb71e0eaea93a7ef1479747c4a00774a
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-10 12:36:58 +0300

    added password reset and update password path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,12 +11,14 @@ const routes: Routes = [
       {path:"customers",loadChildren:()=>import("./admin/components/customer/customer.module").then(module=>module.CustomerModule),canActivate: [AuthGuard]},
       {path:"products",loadChildren:()=>import("./admin/components/products/products.module").then(module=>module.ProductsModule),canActivate: [AuthGuard]},
       {path:"orders",loadChildren:()=>import("./admin/components/order/order.module").then(module=>module.OrderModule),canActivate: [AuthGuard]},
-   ],canActivate: [AuthGuard]},
+   ]},
   {path:"",component:HomeComponent},
   {path:"basket",loadChildren:()=>import("./ui/components/baskets/baskets.module").then(module=>module.BasketsModule)},
   {path:"products",loadChildren:()=>import("./ui/components/products/products.module").then(module=>module.ProductsModule)},
   {path:"register",loadChildren:()=>import("./ui/components/register/register.module").then(module=>module.RegisterModule)},
   {path:"login",loadChildren:()=>import("./ui/components/login/login.module").then(module=>module.LoginModule)},
+  {path:"password-reset",loadChildren:()=>import("./ui/components/password-reset/password-reset.module").then(module=>module.PasswordResetModule)},
+  {path:"update-password",loadChildren:()=>import("./ui/components/update-password/update-password.module").then(module=>module.UpdatePasswordModule)},
 ];
 
 @NgModule({
