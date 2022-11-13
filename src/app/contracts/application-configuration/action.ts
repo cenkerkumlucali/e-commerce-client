18713b168ca69aae01c0890bf7c02f3807a381ba
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-13 19:19:37 +0300

    Created Authorize classes Action and Menu

---
+++ src/app/contracts/application-configuration/action.ts
@@ -0,0 +1,6 @@
+export class Action {
+  actionType:string;
+  httpType:string;
+  definition:string;
+  code:string;
+}
---
+++ src/app/contracts/application-configuration/menu.ts
@@ -0,0 +1,6 @@
+import {Action} from "./action";
+
+export class Menu {
+  name:string;
+  actions:Action[];
+}
